1fa4636f4bbd2119d8506662f09695f2027b4aed
alexticona0802 <alex.ticona@lundero.com>
2022-10-24 22:52:35 -0500

    adding restoring table from backup

--- glue_jobs/restore-tables-job.py
+++ glue_jobs/restore-tables-job.py
@@ -8,6 +8,11 @@ from pyspark.sql import SQLContext
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 import boto3
 import json
+import logging
+import pymysql
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Glue Context
 sc=SparkContext.getOrCreate()
@@ -53,6 +58,60 @@ print ("\nRows get from Query: {0} \n".format(df_jobs.count()))
 df_jobs.printSchema()
 print ("\nRows get from Query: {0} \n".format(df_hired_employees.count()))
 df_hired_employees.printSchema()
+
+try:
+    conn = pymysql.connect(host=host, user=user, passwd=passwd, db=database, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+# truncate_query = """USE globant
+# SET FOREIGN_KEY_CHECKS = 0
+# TRUNCATE table departments
+# TRUNCATE table jobs
+# SET FOREIGN_KEY_CHECKS = 1
+
+# SET FOREIGN_KEY_CHECKS = 0
+# TRUNCATE table hired_employees
+# SET FOREIGN_KEY_CHECKS = 1
+# ;"""
+
+# print(truncate_query)
+
+# with conn.cursor() as cur:
+#     cur.execute(truncate_query)
+# conn.commit()
+
+print('----Start Saving')
+# Rules mysql Connection Options
+mysql_options_jobs = {
+    "url": f"jdbc:{engine}://{host}:{port}/{database}",
+    "dbtable": "jobs",
+    "user": user,
+    "password": passwd
+    }
+mysql_options_departments = {
+    "url": f"jdbc:{engine}://{host}:{port}/{database}",
+    "dbtable": "departments",
+    "user": user,
+    "password": passwd
+    }
+mysql_options_hired_employees = {
+    "url": f"jdbc:{engine}://{host}:{port}/{database}",
+    "dbtable": "hired_employees",
+    "user": user,
+    "password": passwd
+    }
+
+glueContext.write_from_options(frame_or_dfc=df_jobs, connection_type="mysql",
+                              connection_options=mysql_options_jobs)
+glueContext.write_from_options(frame_or_dfc=df_departments, connection_type="mysql",
+                              connection_options=mysql_options_departments)
+glueContext.write_from_options(frame_or_dfc=df_hired_employees, connection_type="mysql",
+                              connection_options=mysql_options_hired_employees)
 
 
 print ("OK********************* \n")
