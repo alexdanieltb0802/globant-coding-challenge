0193a85e473025cfce7c674dcc306e79f22e933d
alexticona0802 <alex.ticona@lundero.com>
2022-10-24 21:44:55 -0500

    recover Avro backup data from s3 repository

--- README.md
+++ README.md
@@ -2,20 +2,19 @@ This repository is focused on solving the proposed challenges and storing the re
 
 The main structure is as follows:
 1. glue_jobs:
-  - Historical load script: performs the extraction task from the S3 source and the data load to the Mysql RDS destination
-  - Table backup script
+  - historical-data-migration-job.py: Historical load script to perform the extraction task from the S3 source and the data load to the Mysql RDS destination
+  - backup-tables-job.py: get data from the database and store a backup of each table in Avro format at S3 buckets
   - Table restore script
 
-2. lambda_function:
-  - function in charge of hosting the REST service for frequent data loading.
-  - POST data to https://rn44gc3d99.execute-api.us-east-1.amazonaws.com/dev
+2. lambda_function: POST data to https://rn44gc3d99.execute-api.us-east-1.amazonaws.com/dev
+  - lambda_function.py: function in charge of hosting the REST service for frequent data loading.
   - test_event.json : json file example
 
 3. Sql:
   - Database generation script
-  - View 1: challenge 2, requirement 1
-  - View 2: challenge 2, requirement 2
+  - challenge2_req1.sql: sql script for challenge 2 - requirement 1
+  - challenge2_req2.sql: sql script for challenge 2 - requirement 2
 
 4. res: Folder for all static resources in the project. For example, dashboards, images, etc.
-  - Quicksight graphic for challenge 2, requirement 1
-  - Quicksight graphic for challenge 2, requirement 2
+  - Quicksight graphic for challenge 2 - requirement 1
+  - Quicksight graphic for challenge 2 - requirement 2
---
+++ glue_jobs/restore-tables-job.py
@@ -0,0 +1,58 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+import boto3
+import json
+
+# Glue Context
+sc=SparkContext.getOrCreate()
+sc.setLogLevel("ERROR")
+glueContext = GlueContext(sc)
+spark_session = glueContext.spark_session
+sqlContext = SQLContext(spark_session.sparkContext, spark_session)
+
+print ("starting job********************* \n")
+
+secrets_client = boto3.client('secretsmanager')
+secrets_response = secrets_client.get_secret_value(SecretId='secret-globant-challenge')
+secret = json.loads(secrets_response['SecretString'])
+host = secret['host']
+user = secret['username']
+passwd = secret['password']
+port = secret['port']
+engine = secret['engine']
+# driver = secret['driver']
+database = 'globant'
+
+df_departments = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": ["s3://globant-source-bucket/backup_tables/departments/run-1666643294864-part-r-00000"]},
+    format="avro"
+)
+
+df_jobs = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": ["s3://globant-source-bucket/backup_tables/jobs/run-1666643301384-part-r-00000"]},
+    format="avro"
+)
+
+df_hired_employees = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": ["s3://globant-source-bucket/backup_tables/jobs/run-1666643301384-part-r-00000"]},
+    format="avro"
+)
+
+print ("\nRows get from Query: {0} \n".format(df_departments.count()))
+df_departments.printSchema()
+print ("\nRows get from Query: {0} \n".format(df_jobs.count()))
+df_jobs.printSchema()
+print ("\nRows get from Query: {0} \n".format(df_hired_employees.count()))
+df_hired_employees.printSchema()
+
+
+print ("OK********************* \n")
